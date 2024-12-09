1e74353c5a05975e65492bee377342578a31c00b
Ben Topping <ben.topping@outlook.com>
2024-12-09 18:09:29 +0000

    feat(advent_of_code): day 8 part 2 solution

--- advent_of_code_2024/08/main.go
+++ advent_of_code_2024/08/main.go
@@ -16,6 +16,7 @@ func main() {
 type Node struct {
 	X, Y int
 }
+
 func part1() {
 	// Read the input file
 	file, err := os.Open("inputs/08.txt")
@@ -44,9 +45,11 @@ func part1() {
 		// bit of a hacky way to figure out and store max x
 		max_x = len(line) + 1
 		for n := 0; n < len(line); n++ {
-			if (string(line[n]) == ".") { continue }
-			
-			frequencies[string(line[n])] = append(frequencies[string(line[n])], Node{X: n+1, Y: y_pos})
+			if string(line[n]) == "." {
+				continue
+			}
+
+			frequencies[string(line[n])] = append(frequencies[string(line[n])], Node{X: n + 1, Y: y_pos})
 		}
 		y_pos++
 	}
@@ -57,14 +60,14 @@ func part1() {
 		for _, node := range frequency {
 			for n := 0; n < len(frequency); n++ {
 				// If on the current node skip
-				if (frequency[n] == node) {
+				if frequency[n] == node {
 					continue
 				}
 
 				var anti_node = Node{}
 				// Anti node position is 2 times current node minus paired node
-				anti_node.X = 2 * node.X - frequency[n].X
-				anti_node.Y = 2 * node.Y - frequency[n].Y
+				anti_node.X = 2*node.X - frequency[n].X
+				anti_node.Y = 2*node.Y - frequency[n].Y
 
 				// If anti node is in a valid position add it to the set
 				if anti_node.X > 0 && anti_node.X < max_x && anti_node.Y > 0 && anti_node.Y < y_pos {
@@ -78,4 +81,71 @@ func part1() {
 	fmt.Println("Number of unique antinodes: ", len(anti_nodes))
 }
 
-func part2() {}
+func part2() {
+	// Read the input file
+	file, err := os.Open("inputs/08.txt")
+
+	// If we don't have an input file return an error
+	if err != nil {
+		fmt.Println("Cannot find input file, please add your input file to inputs/08.txt")
+		return
+	}
+
+	// Ensure we close the file
+	defer file.Close()
+
+	// File reader helper
+	reader := bufio.NewReader(file)
+
+	// Create a map of total to its component values
+	var frequencies = make(map[string][]Node)
+	var y_pos = 1
+	var max_x = 0
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		// bit of a hacky way to figure out and store max x
+		max_x = len(line) + 1
+		for n := 0; n < len(line); n++ {
+			if string(line[n]) == "." {
+				continue
+			}
+
+			frequencies[string(line[n])] = append(frequencies[string(line[n])], Node{X: n + 1, Y: y_pos})
+		}
+		y_pos++
+	}
+
+	var anti_nodes = make(map[Node]int)
+
+	for _, frequency := range frequencies {
+		for _, node := range frequency {
+			for n := 0; n < len(frequency); n++ {
+				// If on the current node skip
+				if frequency[n] == node {
+					if len(frequency) > 1 { anti_nodes[node] = 1 }
+					continue
+				}
+
+				var anti_node = Node{}
+				// Anti node position is 2 times current node minus paired node
+				anti_node.X = 2*node.X - frequency[n].X
+				anti_node.Y = 2*node.Y - frequency[n].Y
+
+				x_diff := node.X - anti_node.X
+				y_diff := node.Y - anti_node.Y
+				// Whilst in the bounds create a new anti node of the current node + difference
+				for anti_node.X > 0 && anti_node.X < max_x && anti_node.Y > 0 && anti_node.Y < y_pos {
+					anti_nodes[anti_node] = 1
+					anti_node.X = anti_node.X - x_diff
+					anti_node.Y = anti_node.Y - y_diff
+				}
+
+			}
+		}
+	}
+
+	fmt.Println("Number of unique antinodes: ", len(anti_nodes))
+}
